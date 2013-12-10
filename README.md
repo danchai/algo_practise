f7fb94e93dfcba446d60b2bde3d64fd0284dc040
danchai <tengweicai@gmail.com>
2013-12-11 02:25:41 +0800

    基本介绍以及 Codeforces 比赛模板

---
+++ README.md
@@ -0,0 +1,10 @@
+# 这是个啥
+总的来说就是放一下我做算法练习中的用到的代码和数据，以及保存一些比赛的代码和模板。
+
+# 有哪些内容
+## Online Judge
+* codeforces - Codeforces题目解法
+* topcoder - TopCoder比赛代码
+## 其他
+* template.cc - Codeforces比赛代码模板
+* templates - 一些算法实现模板
---
+++ template.cc
@@ -0,0 +1,24 @@
+// =====================================================================================
+// 
+//       Filename:  <+NAME+>.cc
+//    Description:  
+//       Compiler:  g++
+//         Author:  Tengwei Cai (twcai), tengweicai@gmail.com
+// 
+// =====================================================================================
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+#define bzero(a,b) memset((a), 0, (b))
+
+int main()
+{
+  // freopen("<+PREFIX+>sample.in", "r", stdin);
+
+  return 0;
+}
